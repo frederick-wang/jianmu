76d797605ed0a75cec42772f6a21f4e7de8372a0
Zhaoji Wang <hwoam@outlook.com>
2023-02-05 06:10:10 +0800

    feat(action): 将 Action 修改为同步模式，去掉了callback

--- jianmu/action.py
+++ jianmu/action.py
@@ -29,7 +29,7 @@ class SaveDialogReturnValue(TypedDict):
 
 
 class OpenDialogReturnValue(TypedDict):
-    filePath: str
+    filePaths: List[str]
     canceled: bool
 
 
@@ -65,85 +65,151 @@ class OpenExternalOptions(TypedDict):
     '''
 
 
-def show_open_dialog(
-    options: Optional[OpenDialogOptions] = None,
-    callback: Optional[Callable[[OpenDialogReturnValue], None]] = None,
-) -> None:
+def show_open_dialog(options: Optional[OpenDialogOptions] = None) -> OpenDialogReturnValue:
     if options is None:
         options = {}
-    if callback is None:
-        callback = lambda result: None
+    return_value: Optional[OpenDialogReturnValue] = None
+
+    def callback(result: OpenDialogReturnValue) -> None:
+        nonlocal return_value
+        return_value = result
+
     socketio = get_socketio()
     socketio.emit('Action:show-open-dialog', {'args': [options]}, callback=callback)
 
+    while True:
+        if return_value is not None:
+            return return_value
+        socketio.sleep(0.05)  # type: ignore
 
-def show_save_dialog(
-    options: Optional[SaveDialogOptions] = None,
-    callback: Optional[Callable[[SaveDialogReturnValue], None]] = None,
-) -> None:
+
+def show_save_dialog(options: Optional[SaveDialogOptions] = None) -> SaveDialogReturnValue:
     if options is None:
         options = {}
-    if callback is None:
-        callback = lambda result: None
+    return_value: Optional[SaveDialogReturnValue] = None
+
+    def callback(result: SaveDialogReturnValue) -> None:
+        nonlocal return_value
+        return_value = result
+
     socketio = get_socketio()
     socketio.emit('Action:show-save-dialog', {'args': [options]}, callback=callback)
 
+    while True:
+        if return_value is not None:
+            return return_value
+        socketio.sleep(0.05)  # type: ignore
 
-def show_message_box(
-    options: MessageBoxOptions,
-    callback: Optional[Callable[[MessageBoxReturnValue], None]] = None,
-) -> None:
-    if callback is None:
-        callback = lambda result: None
+
+def show_message_box(options: MessageBoxOptions) -> MessageBoxReturnValue:
+    return_value: Optional[MessageBoxReturnValue] = None
+
+    def callback(result: MessageBoxReturnValue) -> None:
+        nonlocal return_value
+        return_value = result
+
     socketio = get_socketio()
     socketio.emit('Action:show-message-box', {'args': [options]}, callback=callback)
 
+    while True:
+        if return_value is not None:
+            return return_value
+        socketio.sleep(0.05)  # type: ignore
 
-def show_error_box(
-    title: str,
-    content: str,
-    callback: Optional[Callable[[], None]] = None,
-) -> None:
-    if callback is None:
-        callback = lambda: None
+
+def show_error_box(title: str, content: str) -> None:
+    can_be_returned = False
+
+    def callback(result: None) -> None:
+        nonlocal can_be_returned
+        can_be_returned = True
+
     socketio = get_socketio()
     socketio.emit('Action:show-error-box', {'args': [title, content]}, callback=callback)
 
+    while True:
+        if can_be_returned:
+            return None
+        socketio.sleep(0.05)  # type: ignore
 
-def show_item_in_folder(full_path: str, callback: Optional[Callable[[], None]] = None) -> None:
-    if callback is None:
-        callback = lambda: None
+
+def show_item_in_folder(full_path: str) -> None:
+    can_be_returned = False
+
+    def callback(result: None) -> None:
+        nonlocal can_be_returned
+        can_be_returned = True
+
     socketio = get_socketio()
     socketio.emit('Action:show-item-in-folder', {'args': [full_path]}, callback=callback)
 
+    while True:
+        if can_be_returned:
+            return None
+        socketio.sleep(0.05)  # type: ignore
 
-def open_path(path: str, callback: Optional[Callable[[str], None]] = None) -> None:
-    if callback is None:
-        callback = lambda error_message: None
+
+def open_path(path: str) -> str:
+    return_value: Optional[str] = None
+
+    def callback(result: str) -> None:
+        nonlocal return_value
+        return_value = result
+
     socketio = get_socketio()
     socketio.emit('Action:open-path', {'args': [path]}, callback=callback)
 
+    while True:
+        if return_value is not None:
+            return return_value
+        socketio.sleep(0.05)  # type: ignore
 
-def open_external(url: str,
-                  options: Optional[OpenExternalOptions] = None,
-                  callback: Optional[Callable[[], None]] = None) -> None:
+
+def open_external(url: str, options: Optional[OpenExternalOptions] = None) -> None:
     if options is None:
         options = {}
-    if callback is None:
-        callback = lambda: None
+    can_be_returned = False
+
+    def callback(result: None) -> None:
+        nonlocal can_be_returned
+        can_be_returned = True
+
     socketio = get_socketio()
     socketio.emit('Action:open-external', {'args': [url, options]}, callback=callback)
 
+    while True:
+        if can_be_returned:
+            return None
+        socketio.sleep(0.05)  # type: ignore
 
-def trash_item(path: str, callback: Optional[Callable[[], None]] = None) -> None:
-    if callback is None:
-        callback = lambda: None
+
+def trash_item(path: str) -> None:
+    can_be_returned = False
+
+    def callback(result: None) -> None:
+        nonlocal can_be_returned
+        can_be_returned = True
+
     socketio = get_socketio()
     socketio.emit('Action:trash-item', {'args': [path]}, callback=callback)
 
+    while True:
+        if can_be_returned:
+            return None
+        socketio.sleep(0.05)  # type: ignore
 
-def beep(callback: Optional[Callable[[], None]] = None) -> None:
-    if callback is None:
-        callback = lambda: None
+
+def beep() -> None:
+    can_be_returned = False
+
+    def callback(result: None) -> None:
+        nonlocal can_be_returned
+        can_be_returned = True
+
     socketio = get_socketio()
     socketio.emit('Action:beep', {'args': []}, callback=callback)
+
+    while True:
+        if can_be_returned:
+            return None
+        socketio.sleep(0.05)  # type: ignore
